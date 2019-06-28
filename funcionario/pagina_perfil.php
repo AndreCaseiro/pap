8dac3864bfe5a65b421feec641d992ea821cf20a
AndreCaseiro <andremfcaseiro@gmail.com>
2019-06-28 14:57:55 +0100

    pagina perfil funcionario

--- funcionario/pagina_perfil.php
+++ funcionario/pagina_perfil.php
@@ -172,11 +172,15 @@ include ($_SERVER['DOCUMENT_ROOT']."/acesso_bd.php"); //script de acesso à base
                     </div>
 
                     <div class="form-group">
-                        <label class="col-lg-5 control-label">Função:</label>
-
-                        <input readonly class="form-control" type="text" id="funcao_funcionario" name="funcao_funcionario" value="<?php echo $row['funcao'];?>">
-
-                    </div>
+                                        <label class="col-lg-5 control-label">Função:</label>
+                                        <input readonly class="form-control" type="text" id="funcao_funcionario" name="funcao_funcionario" value="<?php
+                                            $sql_1 = "SELECT funcao FROM codigo_vencimento WHERE codigo_vencimento.idcodigo_vencimento='".$row['funcao']."'";
+                                            $result_1 = mysqli_query($link,$sql_1);
+                                            while($row_1 = mysqli_fetch_array($result_1)) {
+                                                echo utf8_encode($row_1['funcao']);
+                                            }
+                                        ?>">
+                                    </div>
 
                     <div class="form-group">
                         <label class="col-lg-5 control-label">Endereço:</label>
